7e1e19a1c53276869127d6c3f58df73f85c030b8
GrodrZ <fernandogroder@gmail.com>
2022-08-20 13:56:28 -0300

    feat: add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          required
+          id="outlined-required"
+          label="Username"
+        />
+        <TextField required
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+          />
+      </Box>
     </div>
   );
 }
