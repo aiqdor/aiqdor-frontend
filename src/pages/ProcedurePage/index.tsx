b53cb4fd31c6924264615348dc811ec43f796e39
GrodrZ <fernandogroder@gmail.com>
2022-11-07 17:44:56 -0300

    feat: add procedure page

--- src/pages/ProcedurePage/index.tsx
+++ src/pages/ProcedurePage/index.tsx
@@ -1,15 +1,49 @@
 import React, { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import uuid from "react-uuid";
 import firebase from "firebase/app";
-import { Box, Button, Modal, TextField, Typography } from "@mui/material";
+import {
+    Box,
+    Button,
+    Modal,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TextField,
+    Typography,
+} from "@mui/material";
 import { MainHeader } from "../../components/main-header";
 
 const ProcedurePage = () => {
     const navigate = useNavigate();
     const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [duration, setDuration] = useState("");
+    const [price, setPrice] = useState("");
 
     const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
+    const handleOpen = async (_id: string) => {
+        console.log(_id);
+        if (_id) {
+            const procedure = procedures.find((p) => p.id === _id);
+            if (procedure) {
+                setName(procedure.name);
+                setDescription(procedure.description);
+                setDuration(procedure.duration);
+                setPrice(procedure.price);
+            }
+        } else {
+            setName("");
+            setDescription("");
+            setDuration("");
+            setPrice("");
+        }
+        setOpen(true);
+    };
     const handleClose = () => setOpen(false);
     const styleModal = {
         position: "absolute",
@@ -22,11 +56,14 @@ const ProcedurePage = () => {
         boxShadow: 24,
         p: 4,
     };
-
     const handleSubmit = (e: any) => {
         e.preventDefault();
         firebase.firestore().collection("procedures").add({
+            id: uuid(),
             name,
+            description,
+            duration,
+            price,
         });
         setOpen(false);
         getProcedures();
@@ -56,14 +93,16 @@ const ProcedurePage = () => {
     }, []);
 
     return (
-        <div>
+        <Box>
             <MainHeader>
                 <Button variant="contained" onClick={() => navigate("/")}>
-                    Back
+                    Voltar
                 </Button>
             </MainHeader>
 
-            <Button variant="contained" onClick={handleOpen}>Adicionar novo procedimento</Button>
+            <Button variant="contained" onClick={handleOpen}>
+                Adicionar novo procedimento
+            </Button>
             <Modal
                 open={open}
                 onClose={handleClose}
@@ -87,42 +126,126 @@ const ProcedurePage = () => {
                         <TextField
                             required
                             id="outlined-required"
-                            label="Primeiro Nome"
+                            label="Nome"
                             type="text"
-                            autoComplete="given-name"
                             value={name}
                             onChange={(e) => setName(e.target.value)}
                         />
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Descrição"
+                            type="text"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Preço"
+                            type="text"
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
+                        />
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Duração (minutos)"
+                            type="number"
+                            value={duration}
+                            onChange={(e) => setDuration(e.target.value)}
+                        />
                         <Button variant="contained" type="submit">
-                            Cadastrar
+                            Salvar
                         </Button>
                     </Box>
                 </Box>
             </Modal>
 
-            {/* list of procedures */}
-            <h1>Procedures</h1>
-            <div>
-                {procedures.map((procedure) => (
-                    <div>
-                        <h2>{procedure.name}</h2>
-                        <p>{procedure.description}</p>
-                        <p>{procedure.price}</p>
-                        <p>{procedure.duration}</p>
-                        <Button variant="contained" onClick={handleOpen}>
-                            Editar
-                        </Button>
-                        <Button
-                            variant="contained"
-                            onClick={() => deleteProcedure(procedure.uid)}
+            <h1>Procedimentos</h1>
+            <Box
+                sx={{ pt: 3 }}
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+            >
+                {
+                    <TableContainer component={Paper}>
+                        <Table
+                            sx={{ minWidth: 650 }}
+                            size="small"
+                            aria-label="a dense table"
                         >
-                            Deletar
-                        </Button>
-                    </div>
-                ))}
-            </div>
-        </div>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Nome</TableCell>
+                                    <TableCell align="right">
+                                        Descrição
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        Preço&nbsp;(R$)
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        Duração&nbsp;(min)
+                                    </TableCell>
+                                    <TableCell align="right"></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {procedures.map((procedure) => (
+                                    <TableRow
+                                        key={procedure.id}
+                                        sx={{
+                                            "&:last-child td, &:last-child th":
+                                                { border: 0 },
+                                        }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {procedure.name}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {procedure.description}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {procedure.price}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {procedure.duration}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            <Button
+                                                size="small"
+                                                color="primary"
+                                                onClick={() =>
+                                                    handleOpen(procedure.id)
+                                                }
+                                            >
+                                                Editar
+                                            </Button>
+                                            <Button
+                                                size="small"
+                                                color="primary"
+                                                onClick={() =>
+                                                    deleteProcedure(
+                                                        procedure.id
+                                                    )
+                                                }
+                                            >
+                                                Deletar
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                }
+            </Box>
+        </Box>
     );
 };
 
 export default ProcedurePage;
+function uuidv4(): any {
+    throw new Error("Function not implemented.");
+}
