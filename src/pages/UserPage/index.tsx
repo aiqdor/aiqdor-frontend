6c99fc61f8a8e1cf185021743cc3b58ad1a69630
FeGroders <fernandogroder@gmail.com>
2022-11-30 12:59:35 -0300

    feat: show next appointments

--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -1,12 +1,53 @@
-import { useContext } from "react";
-import { Box, Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { Box, Button, Link, Icon, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/auth";
 import { MainHeader } from "../../components/main-header";
+import firebase from "firebase";
+import { Appointment } from "../../types/Appointment";
+
+
 
 const UserPage = () => {
     const { isOwner } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [appointments, setAppointments] = useState<Appointment[]>([]);
+
+    const getAppointments = async () => {
+        const appointments = await firebase
+            .firestore()
+            .collection("slots")
+            .where("user", "==", firebase.auth().currentUser?.uid)
+            .get();
+        const appointmentsData = appointments.docs.map((doc) => ({
+            clinic: doc.data().clinic,
+            date: doc.data().date,
+            user: doc.data().user,
+            day: doc.data().time_slot.day,
+            month: doc.data().time_slot.month,
+            text: doc.data().time_slot.text,
+            // clinic_address: '',
+            // clinic_name: '',
+        })) as Appointment[];
+
+        setAppointments(appointmentsData);
+
+        // load clinics info from appointments
+        appointmentsData.forEach(async (appointment) => {
+            const clinic = await firebase.firestore().collection("clinics").doc(appointment.clinic).get();
+            const clinicData = clinic.data();
+            if (clinicData) {
+                appointment.clinic_name = clinicData.name;
+                appointment.clinic_address = `${clinicData?.state}, ${clinicData?.city}, ${clinicData?.street}, ${clinicData?.addressNumber}`;
+            }
+            setAppointments([...appointmentsData]);
+        });
+
+    };
+
+    useEffect(() => {
+        getAppointments();
+    }, []);
 
     return (
         <div>
@@ -36,6 +77,51 @@ const UserPage = () => {
                         {"Clinicas"}
                     </Button>
                 ) : null}
+
+                {/* PROXIMOS HORARIOS */}
+                <Box>
+                    <h2>Próximos Horários</h2>
+                    <TableContainer component={Paper}>
+                    <Table 
+                        sx={{ minWidth: 650 }} 
+                        aria-label="a dense table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Clínica</TableCell>
+                                <TableCell align="right">Data</TableCell>
+                                <TableCell align="right">Horário</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {appointments.map((appointment) => (
+                                <TableRow
+                                    key={appointment.date}
+                                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {appointment.clinic_name}
+                                        
+                                        <Link href={`https://www.google.com/maps/search/?api=1&query=${encodeURI(
+                                                appointment.clinic_address
+                                            )}`}
+                                            target={"_blank"}
+                                        >
+                                            <Tooltip title="Abrir no mapa">
+                                                <Icon>open_in_new</Icon>
+                                            </Tooltip>
+                                        </Link>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        {appointment.text} - {appointment.day}/{appointment.month}
+                                    </TableCell>
+                                    <TableCell align="right">{appointment.date}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                    </TableContainer>
+                </Box>
+                    
             </Box>
         </div>
     );
---
+++ src/types/Appointment.ts
@@ -0,0 +1,10 @@
+export type Appointment = {
+    clinic: string;
+    clinic_name: string;
+    clinic_address: string;
+    user: string;
+    date: string;
+    day: string;
+    month: string;
+    text: string;
+}
